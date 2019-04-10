670e83f7b2dc927ee182862112a6aa697ca8b952
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 11:04:25 -0400

    Imports 'ReactDOM' and 'renderer' (from 'react-test-renderer') into App.test.js; and also writes a test case for 'matches snapshot'

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import banana from 'react-test-renderer'; // Step 1: Install this npm module as a dev dependency.
+import ReactDOM from 'react-dom'; // Step 1a: Import 'ReactDOM'.
+import renderer from 'react-test-renderer'; // Step 1b: Install + import this npm module as a dev dependency.
 
 import App from './App';
 
 describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
   // Step 2: Write this test.
   it('matches snapshot', () => {
-    const tree = banana.create(<App />); // generates a DOM tree
+    const tree = renderer.create(<App />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
