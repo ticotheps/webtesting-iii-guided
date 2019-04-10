e437401d96f87a86250f0efe50271ea2a9f8a002
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 11:49:37 -0400

    Creates a state object on <App /> and creates a 'speak()' function below the 'render()' function in <App />

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import './App.css';
 import Speaker from '../Speaker';
 
 class App extends Component {
+  state = {
+    message: 'nothing to say'
+  };
+
+
   render() {
     return (
       <div className="App">
-        <Speaker />
+        <Speaker 
+          message={this.state.message}
+          speak={this.speak}
+        />
       </div>
     );
+  }
+
+  speak = () => {
+    this.setState({ message: 'you are not mocking me' });
   }
 }
 
