74f2d01ad2c9f6a3464414938c6212bb4bcbf29e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-10 11:27:59 -0400

    Creates another 'describe();' statement to demonstrate the use of 'mock' functions in the App.test.js file

--- src/App.test.js
+++ src/App.test.js
@@ -12,10 +12,27 @@ describe('<App />', () => {
   });
 
   // Step 2: Write this test.
-  it.skip('matches snapshot', () => {
+  it('matches snapshot', () => {
     const tree = renderer.create(<App />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  describe('mocking', () => {
+    it('is mocking me', () => {
+      const mock = jest.fn(() => 'hello');
+      // const mock = jest.fn().mockImplementation(() => 'hello');
+      // const mock = jest(); mock.mockReturnValue('hello');
+
+      const actual = mock('smile');
+      // mock('another smile');
+
+      expect(actual).toBe('hello');
+      expect(mock).toHaveBeenCalled();
+      expect(mock).toHaveBeenCalledTimes(1);
+      // expect(mock).toHaveBeenCalledWith('frown'); // => fails b/c it was called with something else
+      expect(mock).toHaveBeenCalledWith('smile');
+    });
   });
 });
